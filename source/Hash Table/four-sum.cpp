20294a7f47721a8aafc65d6f81b0a594124f932c
Ashechol <ashechol@qq.com>
2022-10-06 15:02:05 +0800

    Update 哈希表

--- source/Hash Table/four-sum.cpp
+++ source/Hash Table/four-sum.cpp
@@ -46,18 +46,18 @@ vector<vector<int>> fourSum(vector<int>& nums, int target)
     {
         if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-        int t = target - nums[i];
         for (int j = i + 1; j < nums.size(); j++)
         {
             if (j > i + 1 && nums[j] == nums[j - 1])
                 continue;
 
+            long t = (long)target - nums[i] - nums[j];
             int l = j + 1, r = nums.size() - 1;
             while (l < r)
             {
-                if (nums[j] + nums[l] + nums[r] < t)
+                if (nums[l] + nums[r] < t)
                     l++;
-                else if (nums[j] + nums[l] + nums[r] > t)
+                else if (nums[l] + nums[r] > t)
                     r--;
                 else
                 {
